775648aca9ee1f392c99251bb2d37ac852d6e1a3
Saniya98 <saniyafarheen.98@gmail.com>
2024-08-24 20:21:50 -0500

    built fully functional cart

---
+++ src/pages/contact.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Contact = () => {
+  return <div>
+    <h1>Hello there!! We are here to help you..</h1>
+    My Phone: <a href="+15567298766"> +15567298766</a><br></br>
+    My gmail: <a href="f98@gmail.com"> f98@gmail.com</a>
+
+  </div>;
+};
